7bbe012797404b3094fcadcfce7a8a5dfcb5127d
Tch1b0 <Jpour3006@gmail.com>
2022-08-25 09:30:09 +0200

    remove unused enum

--- src/core/utility.py
+++ src/core/utility.py
@@ -1,6 +1,5 @@
-import io
 import logging
-from enum import Enum, auto
+from enum import Enum
 from subprocess import call
 
 log = logging.getLogger()
@@ -10,14 +9,6 @@ class Emoji(Enum):
     ROCKET = "\U0001F680"
     CHECK_MARK = "\U00002705"
     CROSS_MARK = "\U0000274C"
-
-
-class ImportanceLevel(Enum):
-    VERY_LOW = auto()
-    LOW = auto()
-    MEDIUM = auto()
-    IMPORTANT = auto()
-    URGENT = auto()
 
 
 def time_in_seconds(seconds: float = 0, minutes=0, hours=0, days=0) -> float:
