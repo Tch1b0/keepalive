3ac5f223360f7d077549fee60b27123b0b2c50a1
Tch1b0 <Jpour3006@gmail.com>
2022-09-06 08:12:41 +0200

    process: create class

---
+++ src/process.py
@@ -0,0 +1,22 @@
+import asyncio
+import signal
+
+
+class Process:
+    def __init__(self) -> None:
+        self.is_terminating = False
+        for sig in [signal.SIGTERM, signal.SIGINT]:
+            signal.signal(sig, self.exit_gracefully)
+
+    def exit_gracefully(self, *args):
+        self.is_terminating = True
+
+    async def termination(self):
+        """
+        sleep until the process is being terminated.
+
+        Method does NOT terminate the process like the name might suggest. 
+        The name was chosen because of the syntactic sugar `await process.termination()`
+        """
+        while not self.is_terminating:
+            await asyncio.sleep(.1)
