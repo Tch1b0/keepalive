9680956507b024d86a1c9c62ca4c31b3b029543f
Tch1b0 <Jpour3006@gmail.com>
2022-09-12 17:38:44 +0200

    enhance logs

--- __main__.py
+++ __main__.py
@@ -5,13 +5,19 @@ from src.process import Process
 from src.register_jobs import jobs, bot, metrics
 
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger()
 process = Process()
 
 
 async def main():
+    log.log("starting loops update loops")
     asyncio.create_task(metrics.update_loop())
     asyncio.create_task(bot.update_base_message_loop())
+
+    log.log("starting bot")
     await bot.start()
+
+    log.log("starting jobs")
     await jobs.start()
 
     await process.termination()
