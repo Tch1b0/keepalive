4a6e16d52c626e8a4dfe603f043bd577ba614996
tch1b0 <jpour3006@gmail.com>
2022-09-16 18:21:57 +0200

    add docstring to `time_in_seconds` function

--- src/core/utility.py
+++ src/core/utility.py
@@ -10,7 +10,19 @@ class Emoji:
     CROSS_MARK = "\U0000274C"
 
 
-def time_in_seconds(seconds: float = 0, minutes: float = 0, hours: float = 0, days: float = 0) -> float:
+def time_in_seconds(
+    seconds: float = 0, minutes: float = 0, hours: float = 0, days: float = 0
+) -> float:
+    """
+    converts the given time into seconds
+
+    :examples:
+    ```py
+    time_in_seconds(minutes=1)          # => 60
+    time_in_seconds(hours=1)            # => 3600
+    time_in_seconds(minutes=3, hours=2) # => 7330
+    ```
+    """
     return seconds + minutes * 60 + hours * 60 * 60 + days * 60 * 60 * 24
 
 
@@ -18,6 +30,6 @@ def exec_sh(command: str) -> tuple[int, str]:
     """
     execute shell command
     """
-    log.info(f"Executing shell command \"{command}\"")
+    log.info(f'Executing shell command "{command}"')
     code = call(command, shell=True)
     return code
